447a34483becd168c71818572361f3c7de8f6c95
Marvin <35631849+WildcatKSS@users.noreply.github.com>
2018-03-16 17:54:17 +0100

    Update RSI-DEMA.js

--- strategies/RSI-DEMA.js
+++ strategies/RSI-DEMA.js
@@ -1,7 +1,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2018 Marvin
+# Copyright (c) 2018 WildcatKSS
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
