939c3f8b84794fdad66ff265f26d70d867441041
Marvin <35631849+WildcatKSS@users.noreply.github.com>
2018-03-21 17:14:04 +0100

    Update RSI-DEMA.js

--- strategies/RSI-DEMA.js
+++ strategies/RSI-DEMA.js
@@ -86,7 +86,7 @@ method.log = function(candle) {
 
 // Based on the newly calculated information, check if we should but or sell.
 method.check = function(candle) {
-        if(this.rsi < this.RSIlow && this.dema > this.DEMAup) {
+        if(this.rsi > this.RSIhigh && this.dema > this.DEMAup) {
                 // New trend detected
                 if(this.trend.direction !== 'high')
                         this.trend = {
@@ -111,7 +111,7 @@ method.check = function(candle) {
                         this.advice();
                 }
         
-        } else if(this.rsi > this.RSIhigh && this.dema < this.DEMAdown) {
+        } else if(this.rsi < this.RSIlow && this.dema < this.DEMAdown) {
                 // New trend detected
                 if(this.trend.direction !== 'low')
                         this.trend = {
