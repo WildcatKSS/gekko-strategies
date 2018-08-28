c0251107f1f875b739375f75ff8a8a50634a9503
Marvin <35631849+WildcatKSS@users.noreply.github.com>
2018-08-28 14:53:04 -0500

    Update MACD-TS.js

--- strategies/MACD-TS.js
+++ strategies/MACD-TS.js
@@ -1,8 +1,8 @@
 /*
 
-  MIT License
+ 	MIT License
 
-  Copyright (c) 2018 Spiros Koutsopodiotis spiros@primous.org
+ 	Copyright (c) 2018 Spiros Koutsopodiotis spiros@primous.org
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
@@ -10,10 +10,10 @@
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
 	
-  The above copyright notice and this permission notice shall be included in all
+  	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
 	
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -26,16 +26,14 @@
 // helpers
 var _ = require('lodash');
 var log = require('../core/log.js');
-var CCI = require('./indicators/MACD.js');
-var CCI = require('./indicators/CCI.js');
-var RSI = require('./indicators/RSI.js');
+var MACD = require('./indicators/MACD.js');
 
 // let's create our own method
 var method = {};
 
 // prepare everything our method needs
 method.init = function() {
-	this.name = 'MACD-CCI-RSI'; 
+	this.name = 'MACD-TS'; 
 	
 	this.trend = {
 		direction: 'none',
@@ -49,26 +47,16 @@ method.init = function() {
 
 	// define the indicators we need
 	this.addIndicator('macd', 'MACD', this.settings.MACD);
-	this.addIndicator('cci', 'CCI', this.settings.CCI);
-	this.addIndicator('rsi', 'RSI', this.settings.RSI);
 	
 	// define the thresholds
-	this.MACDup = this.settings.thresholds.MACDup;
-    	this.MACDdown = this.settings.thresholds.MACDdown;
-	this.MACDpersistence = this.settings.thresholds.MACDpersistence;
-	this.CCIup = this.settings.thresholds.CCIup;
-	this.CCIdown = this.settings.thresholds.CCIdown;
-	this.CCIpersistence = this.settings.thresholds.CCIpersistence;
-	this.RSIlow = this.settings.thresholds.RSIlow;
-	this.RSIhigh = this.settings.thresholds.RSIhigh;
-	this.RSIpersistence = this.settings.thresholds.RSIpersistence;
+	this.up = this.settings.thresholds.up;
+    	this.down = this.settings.thresholds.down;
+	this.persistence = this.settings.thresholds.persistence;
 }
 
 // what happens on every new candle?
 method.update = function(candle) {
 	this.MACD = this.indicators.macd.result;
-	this.CCI = this.indicators.cci.result;
-	this.RSI = this.indicators.rsi.result;
 }
 
 // for debugging purposes: log the last calculated parameters.
@@ -76,14 +64,12 @@ method.log = function() {
 //    log.debug(`
 //===============================
 // MACD: ${this.MACD}
-// CCI:  ${this.CCI}
-// RSI:  ${this.RSI}
 //===============================
 //`);
 }
 
 method.check = function() {
-	if(this.MACD > this.MACDup && this.CCI > this.CCIup && this.RSI < this.RSIlow) {
+	if(this.MACD > this.up) {
     if(this.trend.direction !== 'up')
 		this.trend = {
 			direction: 'up',
@@ -96,7 +82,7 @@ method.check = function() {
 
     log.info('In uptrend since', this.trend.duration, 'candle(s)');
 
-    if(this.trend.duration >= this.MACDpersistence && this.trend.duration >= this.CCIpersistence && this.trend.duration >= this.RSIpersistence)
+    if(this.trend.duration >= this.persistence)
 		this.trend.persisted = true;
 
     if(this.trend.persisted && !this.trend.adviced) {
@@ -105,7 +91,7 @@ method.check = function() {
     } else
 		this.advice();
 
-	} else if(this.MACD < this.MACDdown && this.CCI < this.CCIdown && this.RSI > this.RSIhigh) {
+	} else if(this.MACD < this.down) {
 	if(this.trend.direction !== 'down')
 		this.trend = {
 			direction: 'down',
@@ -118,7 +104,7 @@ method.check = function() {
 
     log.info('In downtrend since', this.trend.duration, 'candle(s)');
 
-    if(this.trend.duration >= this.MACDpersistence && this.trend.duration >= this.CCIpersistence && this.trend.duration >= this.RSIpersistence)
+    if(this.trend.duration >= this.persistence)
 		this.trend.persisted = true;
 
     if(this.trend.persisted && !this.trend.adviced) {
